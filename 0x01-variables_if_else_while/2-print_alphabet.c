c853f8cc76347989781a97b1e8f05a7150e955bd
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-09 19:24:47 -0700

    0x01. C - Variables, if, else, while Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription:'insomnia number 2'
+ * Return: Always a zero
+ */
+int main(void)
+{
+	int n = 35;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
