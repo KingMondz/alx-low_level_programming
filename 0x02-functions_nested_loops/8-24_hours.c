402fbcb397368c51a4ecb486b2c67559be09c1f3
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-12 06:11:33 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*jack_bauer - fx
+*runs whole day
+*/
+
+void jack_bauer(void)
+{
+int hr, mins;
+
+hr = 0;
+mins = 0;
+
+while (hr <= 23)
+{
+while (mins <= 59)
+{
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(':');
+_putchar((mins / 10) + '0');
+_putchar((mins % 10) + '0');
+_putchar('\n');
+mins++;
+}
+hr++;
+}
+}
