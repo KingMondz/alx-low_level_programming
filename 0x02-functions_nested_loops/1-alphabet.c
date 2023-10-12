3b5c35fe7ec1612acb5461d8acbf4a436fc930d8
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-12 00:54:01 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_alphabet - Starting point
+*Description: 'outputs alphabet in lower case'
+* @void: describe the parameter
+*Return: Issa zero
+*/
+void print_alphabet(void)
+{
+
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+
+_putchar('\n');
+
+}
