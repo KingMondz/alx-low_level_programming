837b1f776f056ddf10a4935668b53544db0f10ea
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-12 06:35:50 -0700

    num 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*add - adds the numbers up
+*@int1: first int
+*@int2: second int
+*Return: the sum
+*/
+
+int add(int int1, int int2)
+{
+int sum = int1 + int2;
+return (sum);
+}
