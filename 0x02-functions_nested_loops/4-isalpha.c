32ae7d7857d99d7defa465447e904bdea27fed0a
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-12 02:34:32 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_isalpha - Starting point
+*@c: what to be checked
+*Desription:checks if character isalpha
+*Return: 1 if c isalpha, 0 otherwise
+*/
+int _isalpha(int c)
+{
+
+if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c >= 'Z')))
+	return (1);
+else
+	return (0);
+
+}
