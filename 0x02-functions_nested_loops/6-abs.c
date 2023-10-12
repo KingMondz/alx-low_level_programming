8e60c78d4b5334d065774998fd634fac99388656
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-12 04:41:35 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_abs- Starting point
+*@n: will be absolute where it likes it or not
+*Desription: return of absolute
+*Return: the absolut value
+*/
+int _abs(int n)
+{
+
+if (n > 0)
+{
+return (1);
+}
+else
+{
+return (-n);
+}
+
+}
