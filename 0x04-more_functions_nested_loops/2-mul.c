5e108b4699ebaec95e6be4169b3aa392f2de2c61
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-12 11:30:36 -0700

    1. _isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*mul - multiplies the numbers up
+*@a: first int
+*@b: second int
+*Return: the multiple value
+*/
+
+int mul(int a, int b)
+{
+int multi = a * b;
+return (multi);
+}
