94e8e4059758318dd47a6d17f17ce834c27dca72
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-12 03:10:00 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,8 +9,8 @@
 int _isalpha(int c)
 {
 
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'))
-	return (1);
-else
+if ((c < 'a' && c > 'z') || (c < 'A' && c > 'Z'))
 	return (0);
+else
+	return (1);
 }
