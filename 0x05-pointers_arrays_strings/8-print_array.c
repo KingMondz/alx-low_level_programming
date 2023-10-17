162f59bae5c3b6179e91e23d3c35429bda87f47f
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-17 03:40:38 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,17 @@ void print_array(int *a, int n)
 {
 int pos;
 
-for (pos = 0; pos < (n - 1); pos++)
+for (pos = 0; pos < n; pos++)
 {
-printf("%d ", a[pos]);
+if (pos != (n - 1))
+{
+printf("%d", a[pos]);
+printf(", ");
 }
-if (pos == (n - 1))
+else
 {
-printf("%d", a[pos - 1]);
+printf("%d", a[pos]);
+}
 }
 putchar('\n');
 }
