692cc130f885398e455db3ec5d8ce48e3fcb8f4f
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-17 09:23:20 -0700

    11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,28 +7,21 @@
 * main - my function
 * Return: is zerooo..Zerooo
 */
+
 int main(void)
 {
-int passuwad[101];
-int incr, sum, num;
 
-sum = 0;
+	int sum;
+	char j;
 
-srand(time(NULL));
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		j = rand() % 128;
+		sum += j;
+		putchar(j);
+	}
+	putchar(2772 - sum);
+	return (0);
 
-for (incr = 0; incr <= 101; incr++)
-{
-passuwad[incr] = rand() % 19;
-sum += (passuwad[incr] + '0');
-putchar(passuwad[incr] + '0');
-
-if ((3191 - sum) -'0' < 19)
-{
-num = 3191 - sum - '0';
-sum += num;
-putchar(num + '0');
-break;
-}
-}
-return (0);
 }
