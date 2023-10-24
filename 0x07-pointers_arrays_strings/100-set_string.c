2d56f99304a8cd4efe92c8a7617feb43b1956166
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-23 18:37:41 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* set_string - our fx
+* @s: var1
+* @to: it matters
+* Return: a function that sets the value
+* of a pointer to a char
+*/
+
+void set_string(char **s, char *to)
+{
+
+*s = to;
+
+}
