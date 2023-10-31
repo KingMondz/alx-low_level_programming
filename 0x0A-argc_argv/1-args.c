0c69e81828e2490815b7b0a71734d936e8a8715b
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-31 13:03:21 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - my function
+* @argc: number of commands
+* @argv: the actual commands
+* Return: zeroo
+*/
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
