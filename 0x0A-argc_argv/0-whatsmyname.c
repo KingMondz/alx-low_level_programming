c4b7440d50f4f33e99b4dd84669f65544dca2892
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-31 11:29:48 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* main - my function
+* @argc: number of commands
+* @argv: the actual commands
+*/
+
+int main(int argc _attribute_((unused)), char *argv[])
+{
+_putchar("%s\n", *argv);
+return (0);
+}
