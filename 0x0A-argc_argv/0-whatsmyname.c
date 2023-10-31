1f4f4db013fd8dd52320a8c3a3bb1d9e60ab5939
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-31 12:51:49 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+
 
 /**
 * main - my function
@@ -9,8 +8,10 @@
 * Return: zeroo
 */
 
-int main(int argc  __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return (0);
+(void) argc;
+
+printf("%s\n", argv[0]);
+return (0);
 }
